33151bcedff5c5ce6bdac29ba97d7d1db5119133
Erich Kauffman <erichkkauffman@gmail.com>
2024-01-06 10:54:13 -0600

    Input query as Text

--- src/Database.hs
+++ src/Database.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Database
   ( Database,
     read,
@@ -8,21 +10,27 @@ where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Reader (ReaderT (..))
-import Database.SQLite.Simple (Connection, FromRow, Query)
+import Data.Text (Text)
+import Database.SQLite.Simple (Connection, FromRow, Query (..))
 import qualified Database.SQLite.Simple as Simple
 import Prelude hiding (read)
 
 newtype Database a = Database (ReaderT Connection IO a)
 
 instance Functor Database where
+  fmap :: (a -> b) -> Database a -> Database b
   fmap f (Database dbReader) = Database $ fmap f dbReader
 
 instance Applicative Database where
+  pure :: a -> Database a
   pure a = Database $ pure a
+
+  (<*>) :: Database (a -> b) -> Database a -> Database b
   (Database dbReaderAtoB) <*> (Database dbReaderA) =
     Database $ dbReaderAtoB <*> dbReaderA
 
 instance Monad Database where
+  (>>=) :: Database a -> (a -> Database b) -> Database b
   (Database dbReaderA) >>= f = Database $ do
     a <- dbReaderA
     let (Database dbReaderB) = f a
@@ -38,8 +46,8 @@ runIO dbFile (Database dbReader) = do
 runLiftIO :: MonadIO m => String -> Database a -> m a
 runLiftIO dbConnection = liftIO . runIO dbConnection
 
-read :: FromRow a => Query -> Database [a]
+read :: FromRow a => Text -> Database [a]
 read query =
   Database $
     ReaderT $
-      \dbConnection -> Simple.query_ dbConnection query
+      \dbConnection -> Simple.query_ dbConnection $ Query query
